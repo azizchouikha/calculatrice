c828da961c0286f87053a9d20b13628a393488b1
aziz chouikha <ahmedaziz.chouikha@ecoles-epsi.net>
2024-03-07 11:49:20 +0100

    2 test vert (valid) "add peut prendre plus de deux nombres en entrée"

--- calculatrice/StringCalculator.cs
+++ calculatrice/StringCalculator.cs
@@ -5,43 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+using System;
+
 namespace calculatrice
 {
     public class StringCalculator
     {
         public int Add(string numbers)
         {
-            
             if (string.IsNullOrEmpty(numbers))
             {
                 return 0;
             }
 
-            
             string[] parts = numbers.Split(',');
-
-            
             int sum = 0;
 
-            
-            for (int i = 0; i < parts.Length; i++)
+            foreach (string part in parts)
             {
-                
-                string currentNumberString = parts[i];
-                int currentNumber;
-
-                
-                bool isNumber = int.TryParse(currentNumberString, out currentNumber);
-
-                
-                if (isNumber)
+                try
                 {
+                    int currentNumber = int.Parse(part);
                     sum += currentNumber;
                 }
-                
-                else
+                catch (FormatException)
                 {
-                    continue;
+                    
+                    throw new FormatException($"La partie '{part}' n'est pas un nombre valide.");
                 }
             }
 
@@ -49,5 +39,6 @@ namespace calculatrice
         }
     }
 }
+
 
 
