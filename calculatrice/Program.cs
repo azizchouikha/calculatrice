ba449d273d9f5f23e95b5fbe9ef4b00a791a0274
aziz chouikha <ahmedaziz.chouikha@ecoles-epsi.net>
2024-03-07 09:51:57 +0100

    1-Créez une méthode int Add(string numbers) prenant un string de forme « x,y » et renvoyant l’entier x+y.

--- calculatrice/Program.cs
+++ calculatrice/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using calculatrice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entrez deux nombres séparés par une virgule:");
+        string input = Console.ReadLine();
+
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.Add(input);
+
+        Console.WriteLine($"Le résultat est: {result}");
+    }
+}
+
---
+++ calculatrice/StringCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatrice
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            string[] parts = numbers.Split(',');
+            return int.Parse(parts[0]) + int.Parse(parts[1]);
+        }
+    }
+
+}
