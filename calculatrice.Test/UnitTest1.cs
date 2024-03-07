9d6be88e0e7b8bbfabf9aa94a4708a5fed7d2e32
aziz chouikha <ahmedaziz.chouikha@ecoles-epsi.net>
2024-03-07 10:11:25 +0100

    1er test vert (valid)

--- calculatrice.Test/UnitTest1.cs
+++ calculatrice.Test/UnitTest1.cs
@@ -1,11 +1,47 @@
+using Xunit;
+using calculatrice;
+
 namespace calculatrice.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Add_ReturnsZero_WhenInputIsEmpty()
         {
+            
+            var calculator = new StringCalculator();
+            var input = "";
 
+            
+            var result = calculator.Add(input);
+
+            
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Add_ReturnsSum_WhenInputIsValid()
+        {
+            
+            var calculator = new StringCalculator();
+            var input = "5,3";
+
+            
+            var result = calculator.Add(input);
+
+            
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void Add_ThrowsFormatException_WhenInputIsInvalid()
+        {
+            
+            var calculator = new StringCalculator();
+            var input = "a,b";
+
+            
+            Assert.Throws<FormatException>(() => calculator.Add(input));
         }
     }
 }
